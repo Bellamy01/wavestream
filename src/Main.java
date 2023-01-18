31c882e4488d7e6900cf1cb577156722f5664022
Bellamy <bdanbellamy@gmail.com>
2023-01-18 22:16:07 +0200

    method query albums by artists

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import model.Artist;
+import model.DataSource;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        DataSource ds = new DataSource();
+        ds.open();
+        if(!ds.open()){
+            System.out.println("Couldn't open the database...");
+        }
+        List<Artist> artists = ds.queryArtists();
+        if(artists == null){
+            System.out.println("Artists not found!");
+        }
+        System.out.println("===========ARTIST DETAILS=============");
+        for(Artist artist : artists){
+            System.out.println(artist.getId()+ " ----- "+artist.getName());
+        }
+        ds.close();
+    }
+}
---
+++ src/model/DataSource.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataSource {
+    private Connection connection;
+    public static final String DB_NAME = "sound.db";
+    public static final String DB_CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\BELLAMY\\IdeaProjects\\wavestream\\"+DB_NAME;
+
+    //artists
+    public static final String TABLE_ARTISTS = "artists";
+    public static final String COLUMN_ARTIST_ID = "_id";
+    public static final String COLUMN_ARTIST_NAME = "name";
+
+    //songs
+    public static final String TABLE_SONGS = "songs";
+    public static final String COLUMN_SONG_ID = "_id";
+    public static final String COLUMN_SONG_TRACK = "track";
+    public static final String COLUMN_SONG_TITLE = "title";
+    public static final String COLUMN_SONG_ALBUM = "album";
+
+    //albums
+    public static final String TABLE_ALBUMS= "songs";
+    public static final String COLUMN_ALBUM_ID = "_id";
+    public static final String COLUMN_ALBUM_NAME = "name";
+
+    public boolean open(){
+        try {
+            connection = DriverManager.getConnection(DB_CONNECTION_STRING);
+            System.out.println("Connection to the the database successful...");
+            return true;
+        } catch (SQLException e){
+            System.out.println("Connection to database failed..."+e.getMessage());
+            return false;
+        }
+    }
+
+    public void close(){
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch(SQLException e){
+            System.out.println("Unable to close connection..."+e.getMessage());
+        }
+    }
+    
+    public List<Artist> queryArtists() {
+        try (Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM "+TABLE_ARTISTS)){
+
+            List<Artist> artists = new ArrayList<>();
+            while(rs.next()) {
+                Artist artist = new Artist();
+
+                int id= rs.getInt(COLUMN_ARTIST_ID);
+                String name = rs.getString(COLUMN_ARTIST_NAME);
+
+                artist.setId(id);
+                artist.setName(name);
+
+                artists.add(artist);
+            }
+            return artists;
+        } catch (SQLException e){
+            System.out.println("Failed to query..."+e.getMessage());
+            return null;
+        }
+    }
+}
